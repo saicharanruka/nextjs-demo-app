fc966f15a3fcf9f2c76740a0ce93623ea7a24bbe
slayer0x10 <saicharanruka@gmail.com>
2025-04-29 12:57:35 +0530

    disabled eslint

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: false
-  // experimental: {
-  //   ppr: 'incremental'
-  // }
+	devIndicators: false,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	// experimental: {
+	//   ppr: 'incremental'
+	// }
 };
 
 export default nextConfig;
